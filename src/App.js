31a7b620e72ff09ff4bc6def8117c320516e707d
SabinShree <kharelsabin467@gmail.com>
2019-12-20 12:44:03 +0545

    added  color in components.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "./App.css";
 import TableUI from "./Components/TableUI";
+import Moment from "moment";
+
 
 function App() {
-  return (
-    <div className="mainApp">
-      <TableUI
-        link="https://jsonplaceholder.typicode.com/comments"
-        removeHeader="body"
-        remove={["postId", "id"]}
-        colorOneCell={{
-          cellName: "email",
-          CompareFromTable: rowValue => {
-            if (rowValue["email"].includes("sydney")) {
-              return true;
-            } else {
-              return false;
-            }
-          },
-          CompareFromOther: () => {}
-        }}
-      />
-    </div>
-  );
+    return (
+        <div className="mainApp">
+            <TableUI
+                title="Task Breakdown"
+                link="http://localhost:8082/customerOnboarding/viewClientInfoWithCitizenship/"
+                removeHeader="address"
+                remove={["id", "phoneNo", "boid", "citizenshipNumber" ]}
+                colorOneCell={{
+                    cellName: "clientName",
+                    CompareFromTable: rowValue => {
+                        return rowValue["clientName"] === "Sabin Kharel";
+                    },
+                    CompareFromOther: () => {
+                    }
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
